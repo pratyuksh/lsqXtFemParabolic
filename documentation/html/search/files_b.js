547f600cbb35db7ceca00cc539e0d9a42b580f9b
pratyuksh <pratyuksh.bansal@gmail.com>
2021-10-12 14:15:56 +0200

    reorganize documentation

--- documentation/html/search/files_b.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['test_5fcases_2ecpp_338',['test_cases.cpp',['../test__cases_8cpp.html',1,'']]],
-  ['test_5fcases_2ehpp_339',['test_cases.hpp',['../test__cases_8hpp.html',1,'']]],
-  ['test_5fcases_5ffactory_2ecpp_340',['test_cases_factory.cpp',['../test__cases__factory_8cpp.html',1,'']]],
-  ['test_5fcases_5ffactory_2ehpp_341',['test_cases_factory.hpp',['../test__cases__factory_8hpp.html',1,'']]]
-];
